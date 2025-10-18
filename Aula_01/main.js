2b17e73bbce91718623b8efa1cc678dd1b8354a8
Jaciel Castro <jacielcastro100@gmail.com>
2025-10-17 21:29:54 -0300

    :books: -- Atualização --

---
+++ Aula_01/main.js
@@ -0,0 +1,13 @@
+import { app, BrowserWindow } from "electron";
+
+const criarjanela = () => {
+    const janela = new BrowserWindow({
+    width: 800,
+    height: 800
+     })
+     janela.loadFile("index.html")
+}
+app.whenReady().then(() => {
+    criarjanela()
+    console.log('realizando Electron !!');
+})
