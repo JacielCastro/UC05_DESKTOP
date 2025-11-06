ec1b7ce4b771e1bcca9745780635230bb73f3781
Jaciel Castro <jacielcastro100@gmail.com>
2025-11-05 21:07:47 -0300

    :books: -- Atualização --

--- Aula_01/main.js
+++ Aula_01/main.js
@@ -1,31 +1,77 @@
-import { app, BrowserWindow } from "electron";
+import { app, BrowserWindow, nativeTheme, ipcMain} from "electron";
+import path from 'node:path'
+import { fileURLToPath } from "node:url"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename) 
+
+let janela = null
 
 const criarjanela = () => {
+    nativeTheme.themeSource = 'light' // MODO CLARO/ESCURO DA JANELA
     const janela = new BrowserWindow({
-    width: 800,
-    height: 800,
-    title: "Exemplo - Aplicação Desktop",
-    webPreferences:{
-        nodeIntegration: false,
-        contextIsolation: true,
-        devTools: true
+        width: 800,
+        height: 800,
+        title: "Exemplo - Aplicação Desktop",
+        webPreferences:{
+            nodeIntegration: false,
+            contextIsolation: true,
+            devTools: true,
+            preload: path.join(__dirname,'preload.js'),
+            sandbox: false,
+            setZoomFactor: 1.0 // deixando o zoom em 100%
     }
     })
     janela.loadFile("./aula02_html/calculadora.html")
     janela.webContents.openDevTools()
     janela.removeMenu()
+    janela.webContents.on('did-finish-load',() => {// EVENTO DISPARADO QUANDO A JANELA TERMINA DE CARREGAR
+        janela.webContents.setZoomFactor(1.0)
+    })
 }
+
 app.whenReady().then(() => {
     criarjanela()
-    console.log('Electron funcionando!!');
 })
-.catch((erro)=> {
-    console.error(erro);
-})
-
 
 app.on('window-all-closed',()=>{
     if(process.platform !== 'drawin'){
         app.quit()
     }
-    })
+})
+
+ipcMain.on('mudar-tema', () => { // RECEBA O EVENTO DO RENDERER PARA MUDAR O TEMA 
+   if (nativeTheme.themeSource === 'dark') {
+        nativeTheme.themeSource = 'light'
+   } else {
+        nativeTheme.themeSource ='dark'
+   }
+})
+
+ipcMain.on('mudar-Zoom', () => {
+    let janelaatual = janela.webContents.getZoomFactor()
+    janela.webContents.setZoomFactor(0.1 + janelaatual)
+})
+
+ipcMain.on('mudar-Zoom', () => {
+    let janelaatual = janela.webContents.getZoomFactor()
+    janela.webContents.setZoomFactor(0.1 - janelaatual)
+})
+ipcMain.on('criar-janela', () => {
+    criarJanela()
+})
+ipcMain.handle('calc-soma', (Event, n1, n2) => {
+    return n1+n2
+})
+
+ipcMain.on('envia-msg', (event, msg) => {
+    console.log('Mensagem do Rederer: ', msg)
+    event.reply('devolver-msg', 'Olá')
+})
+
+.catch((erro)=> {
+    console.error(erro);
+})
+
+
+
